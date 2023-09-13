48aeaa046319a9a309772cb63fd4e628ed089dad
Wallace Franco <wallace.franco@outlook.com>
2023-09-13 02:10:22 -0300

    Alteracoes Gerais

---
+++ Arrays.py
@@ -0,0 +1,18 @@
+# Aqui é um arquivo teste
+lista = ['Peso', 'Idade', 'Sexo']
+for x in lista:
+    print(x)
+# Para adicionar itens na Array 'lista' utilizar o append ou insert
+
+lista.append('Nome')
+
+print(lista)
+
+novoElemento = 'Elemento'
+posicao = 0 # Para adicionar no começo
+
+lista.insert(posicao,novoElemento)
+
+print(lista)
+
+
